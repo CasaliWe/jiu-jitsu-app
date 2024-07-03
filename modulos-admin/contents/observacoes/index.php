8e60efcaf89a51a157e0b8fa6c83880549664441
weslei <wesleicasali18@gmail.com>
2024-07-03 17:11:13 -0300

    att

--- modulos-admin/contents/observacoes/index.php
+++ modulos-admin/contents/observacoes/index.php
@@ -8,9 +8,13 @@
     <h6 class="small mb-5">Veja todas as suas <strong>observações</strong> dos seus <strong>treinos</strong>!</h6>
 
     <div>
-        <?php for ($i = count($observacoes) - 1; $i >= 0; $i--) { ?>
-            <p class="py-2 border-bottom">* <?= $observacoes[$i]; ?>;</p>
-        <?php } ?>
+        <?php for ($i = count($observacoes) - 1; $i >= 0; $i--) {
+                if(strpos($observacoes[$i], ';') !== false){
+                    echo "<p class='py-2 border-bottom'>* $observacoes[$i]</p>";
+                }else{
+                    echo "<p class='py-2 border-bottom'>* $observacoes[$i];</p>";
+                }
+        } ?>
     </div>
 </section>
 <!-- OBSERVAÇÕES -->
--- modulos-admin/contents/treinos/php/get-imagens-treino.php
+++ modulos-admin/contents/treinos/php/get-imagens-treino.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Accept, X-Requested-With, Cache-Control, Authorization, Origin');
+
 require '../../../../config/bootstrap.php';
 use Repositories\TreinoRepository;
 
