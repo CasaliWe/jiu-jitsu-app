dc550db2d9979604c6b8399635f26ceb4b45e426
weslei <wesleicasali18@gmail.com>
2024-08-08 15:54:34 -0300

    att

--- modulos-admin/modal-perfil/index.php
+++ modulos-admin/modal-perfil/index.php
@@ -67,6 +67,10 @@
          
       </div>
 
+      <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+      </div>
+
     </div>
   </div>
 </div>
